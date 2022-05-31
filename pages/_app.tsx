ed44a303e120728831b597ea933a288311c46740
ddjr <ddalyjr@gmail.com>
2022-05-31 10:58:38 -0400

    take 3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import { ApolloProvider } from '@apollo/client'
 import { Toaster } from 'react-hot-toast'
 import { SessionProvider } from 'next-auth/react'
-import Header from '../components/Header'
+import Header from '../components/Header/index'
 import client from '../apollo-client'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
