11ea1583dc2c0a3711f3bce6d7e556b290c69e61
ddjr <ddalyjr@gmail.com>
2022-05-31 11:22:09 -0400

    update types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Header from '../components/Header'
+// import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import { Toaster } from 'react-hot-toast'
 import { SessionProvider } from 'next-auth/react'
 import client from '../apollo-client'
-import Header from '../components/Header'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
 	return (
