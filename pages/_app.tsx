948f4942b623d4d72a8ed632d1a67714271752ed
ddjr <ddalyjr@gmail.com>
2022-05-31 11:10:22 -0400

    Changed _app type

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,9 @@ import { ApolloProvider } from '@apollo/client'
 import { Toaster } from 'react-hot-toast'
 import { SessionProvider } from 'next-auth/react'
 import client from '../apollo-client'
-import Header from '../components/Header/index'
+import Header from '../components/Header'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: any) {
 	return (
 		<ApolloProvider client={client}>
 			<SessionProvider session={session}>
